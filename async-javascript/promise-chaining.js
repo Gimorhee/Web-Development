e7f51db5d04f159ebe6d6d3857b31fdc79c6e1e4
Danny <dongyunrhee@gmail.com>
2019-07-17 07:41:13 +0000

    Learned stack, queue, heap and promise chanining

---
+++ async-javascript/promise-chaining.js
@@ -0,0 +1,24 @@
+let count = 0;
+
+function counter() {
+    count++;
+
+    console.log("Count: " + count);
+}
+
+
+function promiseCounter(callback, time) {
+    const result = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(callback());
+        }, time);
+    });
+
+    return result;
+}
+
+promiseCounter(counter, 1000).then(() => {
+    return promiseCounter(counter,2000);
+}).then(() => {
+    return promiseCounter(counter,3000);
+});
